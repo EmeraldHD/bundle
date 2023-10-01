ed121197dcb70511d1c52140b9a0469d2440a691
Possseidon <xpossseidon@gmail.com>
2023-10-01 22:59:43 +0200

    Use latest version of tranquil (#22)

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,16 +1,16 @@
+use anyhow::Result;
 use tranquil::{
     bot::Bot,
     utils::{debug_guilds_from_env, discord_token_from_env},
-    AnyResult,
 };
 
 #[tokio::main]
-async fn main() -> AnyResult<()> {
+async fn main() -> Result<()> {
     dotenv::dotenv().ok();
 
     Bot::new()
         .application_command_update(debug_guilds_from_env()?)
-        .register(ping_module::PingModule::new())?
+        .register(ping_module::PingModule::new())
         .run_until_ctrl_c(discord_token_from_env()?)
         .await
 }
